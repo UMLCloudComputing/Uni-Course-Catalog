af98e45faa2c3b080997b59892fad1d9a79187dd
ultralapse <136134023+ultralapse@users.noreply.github.com>
2024-08-19 10:42:53 -0400

    feat: Lambda API testing (not working)

--- src/app/db.py
+++ src/app/db.py
@@ -1,11 +1,7 @@
 import boto3
 import os
-
-TABLE_NAME = os.getenv("TABLE")
-
-# # set TABLE_NAME to 'TinypassTable' if it is not set
-if TABLE_NAME is None:
-    TABLE_NAME = 'UniCourseCatalogStack-TableUniCourseCatalogStack6BE39F41-G4BYFQ24EUN0'
+from dotenv import load_dotenv
+load_dotenv()
 
 def get_dynamodb_client():
     return boto3.client('dynamodb', region_name='us-east-1')
@@ -14,13 +10,15 @@ def get_dynamodb_client():
 def insert_course(dict):
     dynamodb = get_dynamodb_client()
     response = dynamodb.put_item(
-        TableName=TABLE_NAME,
+        TableName=os.getenv('TABLE_NAME'),
         Item={
             'prefix': {'S': dict['Department']},
             'code': {'S': dict['CatalogNumber']},
             'title': {'S': dict["Title"]},
             'description': {'S': dict["Description"]},
             'credits' : {'S': str(dict["UnitsMinimum"])},
+            'career' : {'S': dict["AcademicCareer"]["Description"]},
+            'requirements' : {'S': dict["EnrollmentRequirements"]},
         }
     )
     if response['ResponseMetadata']['HTTPStatusCode'] == 200:
@@ -28,14 +26,39 @@ def insert_course(dict):
     else:
         return False
     
-def delete_password(user_id, entry_name):
+def get_course(prefix, code):
+    dynamodb = get_dynamodb_client()
+    response = dynamodb.get_item(
+        TableName=os.getenv('TABLE_NAME'),
+        Key={
+            'prefix': {'S': prefix},
+            'code': {'S': code}
+        }
+    )
+    if 'Item' in response:
+        dict = {
+            'Department': response['Item']['prefix']['S'],
+            'CatalogNumber': response['Item']['code']['S'],
+            'Title': response['Item']['title']['S'],
+            'Description': response['Item']['description']['S'],
+            'Credits': response['Item']['credits']['S'],
+            'Career': response['Item']['career']['S'],
+            'Requirements': response['Item']['requirements']['S']
+        }
+        return dict
+    else:
+        return None
+    
+
+## NOT TESTED
+def delete_course(prefix, code):
     # Delete password
     dynamodb = get_dynamodb_client()
     response = dynamodb.delete_item (
-        TableName=TABLE_NAME,
+        TableName=os.getenv('TABLE_NAME'),
         Key= {
-            'UserID' : {'S' : user_id},
-            'EntryName' : {'S' : entry_name}
+            'prefix' : {'S' : prefix},
+            'code' : {'S' : code}
         }
     )
     if response['ResponseMetadata']['HTTPStatusCode'] == 200:
@@ -44,5 +67,6 @@ def delete_password(user_id, entry_name):
         return False
     
 if __name__ == "__main__":
-    dict = {'Id': '008054', 'Department': 'COMP', 'CatalogNumber': '1000', 'Title': 'Media Computing (Formerly 91.100)', 'Description': 'An introductory course to computer programming using multimedia applications such as images, video and audio. Linear data structures representing multimedia data are manipulated with loops and conditionals in the Python language.', 'UnitsMinimum': 3.0, 'UnitsMaximum': 3.0, 'AcademicCareer': {'Value': 1, 'Description': 'Undergraduate', 'XmlValue': 'UGRD'}, 'AcademicGroup': 'SCI', 'AcademicOrganization': 'LCOMPSCI', 'EnrollmentRequirements': '', 'RequirementDesignation': {'Value': 0, 'Description': 'Any', 'XmlValue': ''}, 'Components': None}
-    print(insert_course(dict))
+    print(get_course('COMP', '1000'))
+    # dict = {'Id': '008054', 'Department': 'COMP', 'CatalogNumber': '1000', 'Title': 'Media Computing (Formerly 91.100)', 'Description': 'An introductory course to computer programming using multimedia applications such as images, video and audio. Linear data structures representing multimedia data are manipulated with loops and conditionals in the Python language.', 'UnitsMinimum': 3.0, 'UnitsMaximum': 3.0, 'AcademicCareer': {'Value': 1, 'Description': 'Undergraduate', 'XmlValue': 'UGRD'}, 'AcademicGroup': 'SCI', 'AcademicOrganization': 'LCOMPSCI', 'EnrollmentRequirements': '', 'RequirementDesignation': {'Value': 0, 'Description': 'Any', 'XmlValue': ''}, 'Components': None}
+    # print(insert_course(dict))
--- src/app/main.py
+++ src/app/main.py
@@ -2,22 +2,8 @@ import json
 import db
 
 def handler(event, context):
-    api_path = event.get('path', '')
-    match api_path:
-        case '/entry':
-            return entry(event)
-        case '/health':
-            return {
-                'statusCode': 200,
-                'body': 'OK'
-            }
-        case _:
-            return {
-                'statusCode': 404,
-                'body': 'Not Found'
-            }
-        
-def entry(event):
+    # api_path = event.get('path', '')
+
     # Get JSON body of HTTP request
     body = json.loads(event.get('body', '{}'))
 
@@ -25,20 +11,20 @@ def entry(event):
     method = event.get('httpMethod', '')
 
     # Get user_id, entry_name, and password from JSON body
-    UserID = body.get('UserID', '')
-    EntryName = body.get('EntryName', '')
-    Password = body.get('Password', '')
+    prefix = body.get('prefix', '')
+    code = body.get('code', '')
+
+    # If no user_id, entry_name, or password, return error
+    if not prefix or not code:
+        return {
+            'statusCode': 400,
+            'body': "You didn't specify any parameters\n"
+        }
 
     match method:
         case 'POST':
-            result = db.upsert_password(UserID, EntryName, Password)
+            result = db.get_course(prefix, code)
             return {
                 'statusCode': 200 if result else 400,
-                'body': 'Password upserted\n' if result else 'Error in upsertion\n'
+                'body': result if result else 'Error in upsertion\n'
             }
-        # case 'DELETE':
-        #     result = db.delete_password(UserID, EntryName)
-        #     return {
-        #         'statusCode': 200 if result else 400,
-        #         'body': 'Password deleted\n' if result else 'Error in deletion\n'
-        #     }
