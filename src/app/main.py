f28656c6d3eee3ccfb2287e472cdbdc3d0fc4fff
ultralapse <136134023+ultralapse@users.noreply.github.com>
2024-08-19 10:55:34 -0400

    chore: fixed bug with API

--- src/app/main.py
+++ src/app/main.py
@@ -3,9 +3,12 @@ import db
 
 def handler(event, context):
     # api_path = event.get('path', '')
+    print("Start of event")
 
     # Get JSON body of HTTP request
-    body = json.loads(event.get('body', '{}'))
+    raw_request = event.get('body', '{}')
+
+    body = json.loads(raw_request)
 
     # What request is it (GET, POST, PUT, DELETE)?
     method = event.get('httpMethod', '')
@@ -15,16 +18,17 @@ def handler(event, context):
     code = body.get('code', '')
 
     # If no user_id, entry_name, or password, return error
-    if not prefix or not code:
-        return {
-            'statusCode': 400,
-            'body': "You didn't specify any parameters\n"
-        }
+    # if not prefix or not code:
+    #     return {
+    #         'statusCode': 400,
+    #         'body': "You didn't specify any parameters\n"
+    #     }
 
     match method:
         case 'POST':
             result = db.get_course(prefix, code)
+            print(result)
             return {
-                'statusCode': 200 if result else 400,
-                'body': result if result else 'Error in upsertion\n'
+                'statusCode': 200,
+                'body': json.dumps(result)
             }
